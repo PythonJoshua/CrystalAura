ea8a68ec2d47c1d97094f0160dee064a83fbec11
pipinstalljosh <70064512+pipinstalljosh@users.noreply.github.com>
2021-01-04 15:39:41 +0000

    Update log.php

--- log.php
+++ log.php
@@ -1,5 +1,5 @@
 <?php
-$logfile= 'index.html';
+$logfile= 'test.html';
 $IP = $_SERVER['REMOTE_ADDR'];
 $logdetails=  date("F j, Y, g:i a") . ': ' . '<a target=_blank href=http://www.dnsstuff.com/tools/ipall.ch?domain='.$_SERVER['REMOTE_ADDR'].'>'.$_SERVER['REMOTE_ADDR'].'</a>';
 $rc = fopen($logfile, "a"); 
